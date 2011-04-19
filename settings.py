b7994da21f3558eed53ec1b8c70a7b7855afc57a
Ian Lawrence <root@ianlawrence.info>
2011-04-19 13:47:22 -0400

    More clean ups :(

--- settings.py
+++ settings.py
@@ -69,8 +69,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'test',                      # Or path to database file if using sqlite3.
-        'USER': 'vern',                      # Not used with sqlite3.
-        'PASSWORD': 'iy_cw6.57',                  # Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         'OPTIONS': {
